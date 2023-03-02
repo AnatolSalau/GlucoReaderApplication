9970a4abd80ecc1c3124bd167b310e015da5af14
AnatolSalau <salovanatoly5@mail.ru>
2023-03-02 10:09:14 +0300

    Initial commit

---
+++ src/main/java/by/delfihealth/salov/glucoreader/GlucoReaderApplication.java
@@ -0,0 +1,13 @@
+package by.delfihealth.salov.glucoreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucoReaderApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(GlucoReaderApplication.class, args);
+      }
+
+}
---
+++ src/test/java/by/delfihealth/salov/glucoreader/GlucoReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package by.delfihealth.salov.glucoreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlucoReaderApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
