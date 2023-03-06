8a33a7f19d6a2f8e9f5f44ffe03d7906a7fec723
AnatolSalau <salovanatoly5@mail.ru>
2023-03-06 16:22:29 +0300

    Monday 6.03.2023

--- src/main/java/by/delfihealth/salov/glucoreader/fx/WindowController.java
+++ src/main/java/by/delfihealth/salov/glucoreader/fx/WindowController.java
@@ -1,11 +1,18 @@
 package by.delfihealth.salov.glucoreader.fx;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.util.Duration;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
@@ -21,6 +28,12 @@ public class WindowController implements Initializable {
       @FXML
       private WebView webView;
 
+      @FXML
+      private StackPane pane;
+
+      @FXML
+      private StackPane splashScreen;
+
 
       private String link = getClass().getResource(
             "/ui/build/index.html"
@@ -30,17 +43,24 @@ public class WindowController implements Initializable {
 
       @Override
       public void initialize(URL location, ResourceBundle resources) {
-            root.setOpacity(0);
             webView.setBlendMode(BlendMode.DARKEN);
             webView.setContextMenuEnabled(false);
             engine = webView.getEngine();
             engine.setJavaScriptEnabled(true);
             engine.load(link);
 
-            double startOpacity = 0;
-            for (int i = 0; i < 100000; i++) {
-                  startOpacity += 0.0001;
-                  root.setOpacity(startOpacity);
-            }
+
+            // Create the initial and final key frames
+            KeyValue initKeyValue = new KeyValue(root.opacityProperty(), 0.0);
+            KeyFrame initFrame = new KeyFrame(Duration.ZERO, initKeyValue);
+            KeyValue endKeyValue = new KeyValue(root.opacityProperty(), 1.0);
+            KeyFrame endFrame = new KeyFrame(Duration.seconds(2), endKeyValue);
+            // Create a Timeline object
+            Timeline timeline = new Timeline(initFrame, endFrame);
+            // Let the animation run forever
+            //timeline.setCycleCount(Timeline.INDEFINITE);
+            // Start the animation
+            timeline.play();
+
       }
 }
