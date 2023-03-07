76d85fabe91e0adc32ac533ba5f6cffe4af3720f
AnatolSalau <salovanatoly5@mail.ru>
2023-03-07 10:42:22 +0300

    Hide splash screen

--- src/main/java/by/delfihealth/salov/glucoreader/fx/WindowController.java
+++ src/main/java/by/delfihealth/salov/glucoreader/fx/WindowController.java
@@ -2,13 +2,13 @@ package by.delfihealth.salov.glucoreader.fx;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
+import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -17,25 +17,18 @@ import org.springframework.stereotype.Component;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.concurrent.TimeUnit;
+import java.util.Stack;
 
 @Component
 public class WindowController implements Initializable {
 
       @FXML
-      private BorderPane root;
-
-      @FXML
       private WebView webView;
 
       @FXML
-      private StackPane pane;
+      private Pane splashScreen;
 
-      @FXML
-      private StackPane splashScreen;
-
-
-      private String link = getClass().getResource(
+      private String linkWindow = getClass().getResource(
             "/ui/build/index.html"
       ).toExternalForm();
 
@@ -47,20 +40,25 @@ public class WindowController implements Initializable {
             webView.setContextMenuEnabled(false);
             engine = webView.getEngine();
             engine.setJavaScriptEnabled(true);
-            engine.load(link);
+            engine.load(linkWindow);
 
 
-            // Create the initial and final key frames
-            KeyValue initKeyValue = new KeyValue(root.opacityProperty(), 0.0);
-            KeyFrame initFrame = new KeyFrame(Duration.ZERO, initKeyValue);
-            KeyValue endKeyValue = new KeyValue(root.opacityProperty(), 1.0);
-            KeyFrame endFrame = new KeyFrame(Duration.seconds(2), endKeyValue);
-            // Create a Timeline object
-            Timeline timeline = new Timeline(initFrame, endFrame);
-            // Let the animation run forever
-            //timeline.setCycleCount(Timeline.INDEFINITE);
-            // Start the animation
-            timeline.play();
+
+            PauseTransition pause = new PauseTransition(Duration.seconds(1));
+            pause.setOnFinished(e -> {
+                                    // Create the initial and final key frames
+                  KeyValue initKeyValue = new KeyValue(splashScreen.opacityProperty(), 1);
+                  KeyFrame initFrame = new KeyFrame(Duration.ZERO, initKeyValue);
+                  KeyValue endKeyValue = new KeyValue(splashScreen.opacityProperty(), 0);
+                  KeyFrame endFrame = new KeyFrame(Duration.seconds(1), endKeyValue);
+                  // Create a Timeline object
+                  Timeline timeline = new Timeline(initFrame, endFrame);
+                  // Let the animation run forever
+                  //timeline.setCycleCount(Timeline.INDEFINITE);
+                  // Start the animation
+                  timeline.play();
+            });
+            pause.play();
 
       }
 }
