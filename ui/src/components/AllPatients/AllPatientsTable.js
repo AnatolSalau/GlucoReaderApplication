a72599116fe9a0e6f27d5e6805119b007acabe97
AnatolSalau <salovanatoly5@mail.ru>
2023-03-10 21:26:30 +0300

    Tuesday 21:26 10.03.2023

--- ui/src/components/AllPatients/AllPatientsTable.js
+++ ui/src/components/AllPatients/AllPatientsTable.js
@@ -15,44 +15,70 @@ function AllPatientsTable() {
                   number: "Номер карты"
             }
       );
-
+/*
       return (
+            <div className={style.table}>
+                  <div className={style.wrapper}>
+                        {
+                              Object.entries(columheaders).map(([columnHeaderKey, columnHeaderValue], index) => {
+                                    return (
+                                          <div>{columnHeaderValue}</div>
+                                    )
+                              })
+                        }
+                        {
+                              allPatients.map((patient, index) => {
+                                    return (
+                                          <div className="table-row">
+                                                {
+                                                      Object.entries(columheaders).map(([columnHeaderKey, columnHeaderValue], index) => {
+                                                            return (
+                                                                  <div>{patient[columnHeaderKey]}</div>
+                                                            )
+                                                      })
+                                                }
+
+                                          </div>
+                                    )
+                              })
+                        }
+                  </div>
+            </div>
+
+      )*/
+
+            return (
                   <div className={style.table}>
                         <div
                               className={style.wrapper}
                         >
-                                    {
-                                          Object.entries(columheaders).map(([columnHeaderKey, columnHeaderValue], index) => {
-                                                return (
-                                                      <div
-                                                            className={style.column}
-                                                            key={index}
-                                                      >
-                                                            <div
-                                                                  className={style.columnHeader}
-                                                            >
-                                                                  {columnHeaderValue}
-                                                            </div>
-                                                            {
-                                                                  allPatients.map((patient, index) => {
-                                                                        return (
-                                                                              <div
-                                                                                    className={style.columnRow}
-                                                                                    key={patient.id}
-                                                                              >
-                                                                                    {patient[columnHeaderKey]}
-                                                                              </div>
-                                                                        )
-                                                                  })
-                                                            }
+                              {
+                                    Object.entries(columheaders).map(([columnHeaderKey, columnHeaderValue], index) => {
+                                          return (
+                                                <div className={style.column} key={index}>
+                                                      <div className={style.columnHeader}>
+                                                            {columnHeaderValue}
                                                       </div>
-                                                )
-                                          })
-                                    }
+                                                      {
+                                                            allPatients.map((patient, index) => {
+                                                                  return (
+                                                                        <div
+                                                                              className={style.columnRow}
+                                                                              key={patient.id}
+                                                                        >
+                                                                              {patient[columnHeaderKey]}
+                                                                        </div>
+                                                                  )
+                                                            })
+                                                      }
+                                                </div>
+                                          )
+                                    })
+                              }
                         </div>
                   </div>
 
-      )
+            )
 }
 
 export default AllPatientsTable;
