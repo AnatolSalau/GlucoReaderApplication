56fb00c959dc697c79360386f35e11dc674fa798
AnatolSalau <salovanatoly5@mail.ru>
2023-03-28 11:16:56 +0300

    Tuesday 28 03 2023 11:16

--- ui/src/components/PatientResultsWindow/PatientResultsWindow.js
+++ ui/src/components/PatientResultsWindow/PatientResultsWindow.js
@@ -1,11 +1,32 @@
 import style from './PatientResultsWindow.module.css'
+import Input from "../Input";
+import {VscTriangleDown} from "react-icons/vsc";
 
 function PatientResultsWindow() {
       return(
             <div
                   className={style.patientsResult}
             >
-                  PatientResultsWindow
+                  <div className={style.wrapper}>
+                        <div className={style.navBar}>
+                              <Input
+                                    textInFront="Ф.И.О пациента: "
+                                    placeholderText="Фамилия Имя Отчество"
+                                    inputWidth={436}
+                              >
+                                    <VscTriangleDown />
+                              </Input>
+                              <Input
+                                    textInFront="Возраст:"
+                                    placeholderText="49"
+                                    inputWidth={165}
+                              >
+                              </Input>
+                        </div>
+                        <div className={style.patientsResultTable}>
+
+                        </div>
+                  </div>
             </div>
       )
 }
