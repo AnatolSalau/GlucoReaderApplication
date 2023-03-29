d8cbb1a319d5cde01ee1ba671ff4cb97a0030dbb
AnatolSalau <salovanatoly5@mail.ru>
2023-03-29 16:16:06 +0300

    Wednesday 29 03 2023 16:16

--- ui/src/components/ChartsWindow/ChartsWindow.js
+++ ui/src/components/ChartsWindow/ChartsWindow.js
@@ -1,11 +1,49 @@
 import style from './ChartsWindow.module.css'
+import Input from "../Input";
 
 function ChartsWindow() {
       return(
             <div
                   className={style.chartsWindow}
             >
-                  ChartsWindow
+                  <div
+                        className={style.wrapper}
+                  >
+                        <div
+                              className={style.navBar}
+                        >
+                              <div
+                                    className={style.navBarRow}
+                              >
+                                    <Input
+                                          textInFront={'Ф.И.О. пациента:'}
+                                          placeholderText={'Фамилия Имя Отчество'}
+                                          inputWidth={426}
+                                    />
+                                    <Input
+                                          textInFront={'Возраст:'}
+                                          placeholderText={'49'}
+                                          inputWidth={165}
+                                    />
+                              </div>
+                              <div
+                                    className={style.navBarRow}
+                              >
+                                    Row2
+                              </div>
+                              <div
+                                    className={style.navBarRow}
+                              >
+                                    Row3
+                              </div>
+                        </div>
+                        <div
+                              className={style.chartCard}
+                        >
+
+                              chartCard
+                        </div>
+                  </div>
             </div>
       )
 }
