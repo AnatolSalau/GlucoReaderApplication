66ebd2b31bd57b9b2a89caeaff2d18ec09057d09
AnatolSalau <salovanatoly5@mail.ru>
2023-04-05 16:24:39 +0300

    Thursday 05.04.2023 05.04.2023

--- ui/src/components/LineChart/LineChart.js
+++ ui/src/components/LineChart/LineChart.js
@@ -3,71 +3,80 @@ import { Chart } from "react-google-charts";
 
 function LineChart() {
       const data = [
-            ["Year", "Sales", "Expenses", "Profit"],
-            ["2014", 1000, 1000, 1000],
-            ["2015", 1000, 1000, 1000],
-            ["2016", 1000, 1000, 1000],
-            ["2017", 1000, 1000, 1000],
+            [
+                  'Month',
+                  'Bolivia',
+                  'Ecuador',
+                  'Madagascar',
+                  'Papua New Guinea',
+                  'Rwanda',
+                  'Average',
+            ],
+            ['2004/05', 4, 4, 4, 4, 4, 4],
+            ['2005/06', 4, 4, 4, 1, 4, 4],
+            ['2006/07', 4, 4, 4, 7, 4, 4],
+            ['2007/08', 4, 4, 4, 10, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
+            ['2008/09', 4, 4, 4, 6, 4, 4],
       ];
 
-      const options = {
-            chart: {
-                  title: "Company Performance",
-                  subtitle: "Sales, Expenses, and Profit: 2014-2017",
-            },
-      };
       const options2 = {
+/*            theme: 'material',*/
+            smoothLine: true,
             chartArea: {
-                  width: '60%'
+/*                  width: '60%'*/
             },
+            tooltip: {isHtml: true},
+            theme: 'material',
             hAxis: {
                   ticks: [0, 15, 30, 45, 60],
-                  title: 'Time'
+                  title: 'Time',
+                  slantedText: true,
             },
             isStacked: true,
             series: {
-                  // low
                   0: {
                         areaOpacity: 0.6,
                         color: '#FFF59D',
-                        visibleInLegend: false
+                        visibleInLegend: false,
                   },
-
-                  // avg
                   1: {
                         areaOpacity: 0.6,
                         color: '#A5D6A7',
                         visibleInLegend: false
                   },
-
-                  // high
                   2: {
                         areaOpacity: 0.6,
                         color: '#EF9A9A',
                         visibleInLegend: false
                   },
-
-                  // dogs
                   3: {
-                        color: '#01579B',
-                        type: 'line'
+                        color: 'red',
+                        type: 'line',
                   }
             },
             seriesType: 'area',
             title: 'Example',
             vAxis: {
-                  ticks: [0, 25, 50, 75, 100],
+                  ticks: [0, 4, 8, 12, 16],
                   title: 'Popularity'
             }
       };
-
       return (
             <div className={style.lineChart}>
-                  New Line Chart
                   <Chart
                         chartType="ComboChart"
-                        width="100%"
-                        height="400px"
+                        width={'100%'}
+                        height={'100%'}
                         data={data}
                         options={options2}
                   />
