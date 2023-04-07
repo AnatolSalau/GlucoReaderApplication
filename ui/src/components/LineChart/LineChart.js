a6b64ab7feeff32fea96973179163568b8590f63
AnatolSalau <salovanatoly5@mail.ru>
2023-04-07 16:22:56 +0300

    Friday 16:22 07.04.2023

--- ui/src/components/LineChart/LineChart.js
+++ ui/src/components/LineChart/LineChart.js
@@ -3,17 +3,15 @@ import {useEffect, useRef, useState} from "react";
 import style from './LineChart.module.css'
 import { Chart } from "react-google-charts";
 
-
 function LineChart() {
-      console.log("Render LineChart")
-      const ref = useRef();
+      console.log("Render LineChart");
+
       const [size, setSize] = useState({
             width: window.innerWidth,
             height: window.innerHeight
       });
 
       const resizeHandler = () => {
-            console.log(ref.current);
             setSize({
                   width: window.innerWidth,
                   height: window.innerHeight
@@ -23,81 +21,91 @@ function LineChart() {
       };
 
       useEffect(() => {
-            console.log(ref.current);
             window.addEventListener("resize", resizeHandler);
             resizeHandler();
-      }, []);
 
+      }, []);
       const data = [
             [
                   'Month',
-                  'Bolivia',
-                  'Ecuador',
-                  'Madagascar',
-                  'Papua New Guinea'
+                  'Ниже нормы',
+                  'Норма',
+                  'Выше нормы',
+                  'Уровень глюкозы (ММОЛЬ/Л)',
+                  'Custom'
             ],
-            [1, 4, 4, 4, 4],
-            [2, 4, 4, 4, 1],
-            [3, 4, 4, 4, 7],
-            [4, 4, 4, 4, 10],
-            [5, 4, 4, 4, 6],
-            [6, 4, 4, 4, 6],
-            [7, 4, 4, 4, 6],
-            [8, 4, 4, 4, 6],
-            [9, 4, 4, 4, 6],
-            [10, 4, 4, 4, 6],
-            [11, 4, 4, 4, 6],
-            [12, 4, 4, 4, 6],
-            [13, 4, 4, 4, 6],
-            [14, 4, 4, 4, 6],
-            [15, 4, 4, 4, 6],
+            [1, 4, 4, 4, 6, {v: 6, f: '11.00\n01.12.2022'}],
+            [2, 4, 4, 4, 1, 6],
+            [3, 4, 4, 4, 7, 6],
+            [4, 4, 4, 4, 10, 6],
+            [5, 4, 4, 4, 6, 6],
+            [6, 4, 4, 4, 6, 6],
+            [7, 4, 4, 4, 6, 6],
+            [8, 4, 4, 4, 6, 6],
+            [9, 4, 4, 4, 6, 6],
+            [10, 4, 4, 4, 6, 6],
+            [11, 4, 4, 4, 6, 6],
+            [12, 4, 4, 4, 6, 6],
+            [13, 4, 4, 4, 6, 6],
+            [14, 4, 4, 4, 6, 6],
+            [15, 4, 4, 4, 6, 6],
       ];
 
       const options = {
+
 /*            theme: 'material',*/
             smoothLine: true,
             chartArea: {
                   // leave room for y-axis labels
+/*                  width: '92%',
+                  height: '65%',
+                  top:25*/
                   width: '92%',
                   height: '75%'
-
             },
             legend: {
+                  textStyle:{
+                        fontSize: 16
+                  },
                   position: 'top',
                   enableInteractivity: false
             },
             width: size.width - 240,
-            height: size.height - 425,
-            tooltip: {isHtml: true},
+            height: size.height - 475,
+            tooltip: {
+                  isHtml: true,
+            },
             theme: 'material',
             hAxis: {
-                  textStyle:{color: 'black'},
+                  textStyle:{
+/*                        color: 'black'*/
+                  },
                   baselineColor: 'white',
                   ticks: [
-                        {v: 1, f: 'Zero'},
-                        {v: 2, f: 'One'},
-                        {v: 3, f: 'Two'},
-                        {v: 4, f: 'Zero'},
-                        {v: 5, f: 'One'},
-                        {v: 6, f: 'Two'},
-                        {v: 7, f: 'Zero'},
-                        {v: 8, f: 'One'},
-                        {v: 9, f: 'Two'},
-                        {v: 10, f: 'Zero'},
-                        {v: 11, f: 'One'},
-                        {v: 12, f: 'Two'},
-                        {v: 13, f: 'Zero'},
-                        {v: 14, f: 'One'},
-                        {v: 15, f: 'Two'}
+                        {v: 'dsd', f: '07.00\n01.12.2022'},
+                        {v: 2, f: '07.28\n01.12.2022'},
+                        {v: 3, f: '07.35\n01.12.2022'},
+                        {v: 4, f: '07.00\n02.12.2022'},
+                        {v: 5, f: '07.28\n02.12.2022'},
+                        {v: 6, f: '07.35\n02.12.2022'},
+                        {v: 7, f: '07.00\n03.12.2022'},
+                        {v: 8, f: '07.28\n03.12.2022'},
+                        {v: 9, f: '07.35\n03.12.2022'},
+                        {v: 10, f: '07.00\n04.12.2022'},
+                        {v: 11, f: '07.28\n04.12.2022'},
+                        {v: 12, f: '07.35\n04.12.2022'},
+                        {v: 13, f: '07.00\n05.12.2022'},
+                        {v: 14, f: '07.28\n05.12.2022'},
+                        {v: 15, f: '07.35\n05.12.2022'}
                   ],
-                  title: 'Time',
-                  slantedText: true,
+                  title: 'ВРЕМЯ/ДАТА',
+/*                  slantedText: true,*/
             },
             isStacked: true,
             series: {
                   0: {
                         areaOpacity: 0.7,
-                        color: '#FFF59D',
+                        color: '#E6F1FD',
                         visibleInLegend: true,
                         tooltip: false,
 /*                        pointSize: 0,
@@ -107,30 +115,41 @@ function LineChart() {
                   },
                   1: {
                         areaOpacity: 0.7,
-                        color: '#A5D6A7',
+                        color: '#D5FCE5',
                         visibleInLegend: true,
                         tooltip: false,
                         enableInteractivity: false
                   },
                   2: {
                         areaOpacity: 0.7,
-                        color: '#EF9A9A',
+                        color: '#FAD8C7',
                         visibleInLegend: true,
                         tooltip: false,
                         enableInteractivity: false
                   },
                   3: {
+                        color: 'black',
+                        type: 'line',
+                        visibleInLegend: false,
+                  },
+/*                  4: {
                         color: 'red',
                         type: 'line',
                         visibleInLegend: false,
-                  }
+                        labelInLegend: 'max.temp.'
+                  }*/
             },
             seriesType: 'area',
 /*            title: 'Example',*/
             vAxis: {
-                  textStyle:{color: 'black'},
-                  ticks: [0, 4, 8, 12],
-                  title: 'Popularity',
+                  textStyle:{
+/*                        color: 'rgba(0, 0, 0, 0.38)'*/
+                  },
+                  ticks: [4, 8, 12],
+                  title: 'ММОЛЬ/Л',
+                  titleTextStyle: {
+/*                        color: 'gray'*/
+                  },
                   baselineColor: 'white',
             }
       };
