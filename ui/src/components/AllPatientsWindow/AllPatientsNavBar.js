8c07bae2e267364869c26021133f6ccde7f048bd
AnatolSalau <salovanatoly5@mail.ru>
2023-04-13 14:45:12 +0300

    Tuesday 14:45 12.04.2023

--- ui/src/components/AllPatientsWindow/AllPatientsNavBar.js
+++ ui/src/components/AllPatientsWindow/AllPatientsNavBar.js
@@ -6,17 +6,35 @@ import style from './AllPatientsNavBar.module.css'
 import Input from "../Input";
 import Button from "../Button";
 import InputWithoutText from "../InputWithoutText";
+import Dropdown from "../Dropdown";
+import {useState} from "react";
 
 function AllPatientsNavBar() {
+      const dropDownItems = ['Линейный график', 'Круговая диаграмма'];
+      const [selectedItem, setSelectedItem] = useState(dropDownItems[0]);
+
       return (
             <div className={style.allPatientsNavBar}>
-                        <Input
-                              textInFront="Врач:"
-                              placeholderText="Фамилия Имя Отчество"
-                              inputWidth={350}
+                  <Input
+                        textInFront="Врач:"
+                        placeholderText="Фамилия Имя Отчество"
+                        inputWidth={350}
+                  >
+                        <VscTriangleDown />
+                  </Input>
+{/*                  <div className={style.dropDownWrapper}>
+                        <div
+                              className={style.dropDownWrapperText}
                         >
-                              <VscTriangleDown />
-                        </Input>
+                              Врач:
+                        </div>
+
+                        <Dropdown
+                              dropDownItems={dropDownItems}
+                              selectedItem={selectedItem}
+                              setSelectedItem = {setSelectedItem}
+                        />
+                  </div>*/}
                   <Button text="Добавить пациента">
                         <FaUserPlus />
                   </Button>
