2e4d8cd7874269b394bbd7b109e8e7dd08d6dc2d
AnatolSalau <salovanatoly5@mail.ru>
2023-06-16 12:43:43 +0300

    Friday 16.06.2023 SetConverterType

--- src/main/java/by/delfihealth/salov/glucoreader/comport/Test.java
+++ src/main/java/by/delfihealth/salov/glucoreader/comport/Test.java
@@ -12,9 +12,7 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
 
- */
 public class Test {
       public static void main(String[] args) {
 /*            getProtocolVersion();
